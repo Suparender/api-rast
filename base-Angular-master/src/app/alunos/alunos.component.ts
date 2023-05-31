72745b5188d262b1a02abac20819f24d242a35be
Suparender <gamesrodrigo094@gmail.com>
2023-05-31 15:15:06 -0300

    atualização
    momento do intervalo

--- base-Angular-master/src/app/alunos/alunos.component.ts
+++ base-Angular-master/src/app/alunos/alunos.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicosService } from '../service/servicos.service';
+import { AlunoModel } from './aluno.model';
 ServicosService
-
+AlunoModel
 @Component({
   selector: 'app-alunos',
   templateUrl: './alunos.component.html',
@@ -9,14 +10,22 @@ ServicosService
 })
 export class AlunosComponent implements OnInit {
 
+  aluno: AlunoModel = new AlunoModel()
+
+  alunos: Array<any> = new Array()
+  
   constructor(private alunosService: ServicosService) {}
 
   ngOnInit(): void {
-    
+    this.Listar()
   }
 
   Listar() {
-    this.alunosService.listarAlunos().subscribe()
+    this.alunosService.listarAlunos().subscribe( alunos => {
+      this.alunos = alunos
+    }, err => {
+      console.log("Erro ao listar alunos", err)
+    } )
   } 
 
 }
